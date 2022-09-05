0b228ad27cf09afe4333235d14bcf6c13e0eaa0e
FFFFFaraway <425499721@qq.com>
2022-09-05 21:13:08 +0800

    format the RV and AE, shorten code

--- src/raft/rpc.go
+++ src/raft/rpc.go
@@ -41,112 +41,59 @@ type AEReply struct {
 func (rf *Raft) RequestVote(args *RequestVoteArgs, reply *RequestVoteReply) {
 	term := <-rf.term
 	reply.Term = term
-	if args.Term > term {
-		Debug(dTerm, rf.me, "<- RV from S%v, newer term:%v", args.CandidateId, args.Term)
-		go func() { rf.term <- args.Term }()
-
-		rf.becomeFollower()
-
-		<-rf.logCh
-		log := rf.log
-		lastLogIndex := len(log)
-		go func() { rf.logCh <- void{} }()
-
-		vote := lastLogIndex == 0
-		vote = vote || args.LastLogTerm > log[lastLogIndex-1].Term
-		vote = vote || (args.LastLogTerm == log[lastLogIndex-1].Term && args.LastLogIndex >= lastLogIndex)
-
-		if lastLogIndex > 0 {
-			Debug(dDrop, rf.me, "args LastTerm: %v, me LastTerm: %v, vote: %v", args.LastLogTerm, log[lastLogIndex-1].Term, vote)
-		}
-
-		if !vote {
-			Debug(dVote, rf.me, "Refuse Vote -> S%v", args.CandidateId)
-			return
-		}
-		reply.Vote = true
-		vf := <-rf.voteFor
-		Debug(dVote, rf.me, "Grant Vote %v -> S%v", vf, args.CandidateId)
-		go func() { rf.voteFor <- args.CandidateId }()
-		return
-	}
-
-	go func() { rf.term <- term }()
-
 	if args.Term < term {
+		go func() { rf.term <- term }()
 		return
 	}
+	go func() { rf.term <- args.Term }()
 
-	go func() { rf.electionTimer <- void{} }()
-
-	vf := <-rf.voteFor
-	// voted for someone else
-	if vf != -1 && vf != args.CandidateId {
-		go func() { rf.voteFor <- vf }()
-		return
-	}
-
+	// grant vote restriction
 	<-rf.logCh
 	log := rf.log
 	lastLogIndex := len(log)
 	go func() { rf.logCh <- void{} }()
 
 	vote := lastLogIndex == 0
-	vote = vote || args.LastLogTerm > rf.log[lastLogIndex-1].Term
-	vote = vote || (args.LastLogTerm == rf.log[lastLogIndex-1].Term && args.LastLogIndex >= len(log))
+	vote = vote || args.LastLogTerm > log[lastLogIndex-1].Term
+	vote = vote || (args.LastLogTerm == log[lastLogIndex-1].Term && args.LastLogIndex >= lastLogIndex)
+
+	vf := <-rf.voteFor
+	if args.Term > term {
+		Debug(dTerm, rf.me, "<- RV from S%v, newer term:%v", args.CandidateId, args.Term)
+		rf.becomeFollower()
+	} else {
+		Debug(dTerm, rf.me, "<- RV from S%v, same term:%v", args.CandidateId, args.Term)
+		go func() { rf.electionTimer <- void{} }()
+		// voted for someone else
+		if vf != -1 && vf != args.CandidateId {
+			vote = false
+		}
+	}
 
 	if !vote {
 		go func() { rf.voteFor <- vf }()
 		Debug(dVote, rf.me, "Refuse Vote -> S%v", args.CandidateId)
 		return
 	}
-
-	Debug(dVote, rf.me, "Grant Vote %v -> S%v, Same Term", vf, args.CandidateId)
+	Debug(dVote, rf.me, "Grant Vote %v -> S%v", vf, args.CandidateId)
 	go func() { rf.voteFor <- args.CandidateId }()
 	reply.Vote = true
-	return
 }
 
 func (rf *Raft) AE(args *AEArgs, reply *AEReply) {
 	term := <-rf.term
 	reply.Term = term
+	if args.Term < term {
+		go func() { rf.term <- term }()
+		return
+	}
+	go func() { rf.term <- args.Term }()
+
 	if args.Term > term {
 		Debug(dTerm, rf.me, "<- AE, newer term:%v", args.Term)
-		go func() { rf.term <- args.Term }()
-
-		rf.becomeFollower()
-
-		<-rf.voteFor
-		go func() { rf.voteFor <- args.LeaderID }()
-
-		// Logs
-		<-rf.logCh
-		if len(rf.log) >= args.PrevLogIndex {
-			if args.PrevLogIndex == 0 || rf.log[args.PrevLogIndex-1].Term == args.PrevLogTerm {
-				rf.log = append(rf.log[:args.PrevLogIndex], args.Logs...)
-				reply.Success = true
-			}
-		}
-		go func() { rf.logCh <- void{} }()
-
-		commitIndex := <-rf.commitIndex
-		if reply.Success && args.CommitIndex > commitIndex {
-			Debug(dApply, rf.me, "<- AE, update commitIndex: %v -> %v", commitIndex, args.CommitIndex)
-			go func() { rf.commitIndex <- args.CommitIndex }()
-		} else {
-			Debug(dDrop, rf.me, "<- AE, refuse update commitIndex: %v -> %v", commitIndex, args.CommitIndex)
-			go func() { rf.commitIndex <- commitIndex }()
-		}
-		return
+	} else {
+		Debug(dTerm, rf.me, "<- AE, same term: %v", args.Term)
 	}
-
-	go func() { rf.term <- term }()
-
-	if args.Term < term {
-		return
-	}
-
-	Debug(dTerm, rf.me, "<- AE, same term: %v", args.Term)
 
 	rf.becomeFollower()
 
@@ -168,7 +115,7 @@ func (rf *Raft) AE(args *AEArgs, reply *AEReply) {
 		Debug(dApply, rf.me, "<- AE, update commitIndex: %v -> %v", commitIndex, args.CommitIndex)
 		go func() { rf.commitIndex <- args.CommitIndex }()
 	} else {
-		Debug(dApply, rf.me, "<- AE, refuse update commitIndex: %v -> %v", commitIndex, args.CommitIndex)
+		Debug(dDrop, rf.me, "<- AE, refuse update commitIndex: %v -> %v", commitIndex, args.CommitIndex)
 		go func() { rf.commitIndex <- commitIndex }()
 	}
 }
