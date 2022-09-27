53c24ae6f7b442befe02aa22ec3cbd41f4f329a7
FFFFFaraway <425499721@qq.com>
2022-09-27 11:56:11 +0800

    update cleaner (pass 4B without challenge1 1280 times)

--- src/shardkv/cleaner.go
+++ src/shardkv/cleaner.go
@@ -1,13 +1,19 @@
 package shardkv
 
+import "time"
+
+const WaitAllDie = 5 * time.Second
+
 func (kv *ShardKV) cleaner() {
 	select {
 	case <-kv.dead:
 		Debug(dTerm, kv.gid-100, "killed")
 		for {
 			select {
+			case <-kv.configCh:
+			case <-kv.mckCh:
 			case <-kv.dataCh:
-			default:
+			case <-timeoutCh(WaitAllDie):
 				return
 			}
 		}
